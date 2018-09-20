3312cd739c64ae3fdfc848f5650b23b3233ea912
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-09-19 22:42:05 -0300

    Add project files.

---
+++ ConsoleNETCoreKafka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace ConsoleNETCoreKafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            OpenProducer().Wait();
+        }
+
+        static async Task OpenProducer()
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:2181" }
+            };
+
+            using (var producer = new Producer<Null, string>(
+                       config,
+                        null,
+                        new StringSerializer(Encoding.UTF8)))
+            {
+                await producer.ProduceAsync("topic", null, "fiap");
+            }
+        }
+
+    }
+}
---
+++ ConsoleNETCoreKafkaConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+
+namespace ConsoleNETCoreKafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var config = new Dictionary<string, object> {
+                                        { "group.id", "test-consumer-group" },
+                                        { "bootstrap.servers", "localhost:2181" },
+                                        { "auto.commit.interval.ms", 5000 },
+                                        { "auto.offset.reset", "earliest" }}; // latest
+
+            using (var consumer = new Consumer<Null, string>(
+            config,
+            null,
+            new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnMessage += (_, msg) => Console.WriteLine(msg.Value);
+                consumer.Subscribe("topic");
+                while (true) { consumer.Poll(TimeSpan.FromMilliseconds(100)); }
+            }
+
+        }
+    }
+}
